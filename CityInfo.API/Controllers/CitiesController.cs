3055dc88da3ddebf1ade7e6a2a154c8323ae7dbb
Carmen <91155317+CarmenUZH@users.noreply.github.com>
2022-07-06 10:46:10 +0200

    Set-up

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    public class CitiesController: ControllerBase
+    {
+        [HttpGet("api/[controller]")] //Specify routes with attributes (see Program.cs)
+        //you can input controller because the name matches (refactoring the name would cause problems later tho!)
+        public JsonResult GetCities()
+        {
+           return new JsonResult(
+                new List<object>
+                {
+                    new { id = 1, Name="Basel" },
+                    new{id = 2, Name="Bern"} 
+                });
+        }
+    }
+}
--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -2,7 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Starting point of our application, Main Methods gets created behind the scenes
 
-builder.Services.AddControllers();
+builder.Services.AddControllers();//Registers necessary services to implement controllers (Remember we're in MVC)
+//Addcontrollesrwithviews would also regiester services for views, but this application is just for api so we dont really need views - Maybe better for ASPtest
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(); //Register required services - The reason things work
 
@@ -16,8 +17,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting(); //Marks the position in the middleware pipeline where a routing decision is made
 app.UseAuthorization(); //Authentication should be added pretty early on 
+app.UseEndpoints(endpoints => //Marks the position in the middleware pipeline where the selected endpoint is executed
+{
+    endpoints.MapControllers();//add endpoints without specifying routes (specify routes with attributes)
+});
 
 app.MapControllers();
 /*
