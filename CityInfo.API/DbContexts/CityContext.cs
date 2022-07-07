d4605592a879ba49dfaeea980a90f9821657204a
Carmen <91155317+CarmenUZH@users.noreply.github.com>
2022-07-07 09:44:50 +0200

    Update CityContext.cs

--- CityInfo.API/DbContexts/CityContext.cs
+++ CityInfo.API/DbContexts/CityContext.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CityInfo.API.DbContexts
 {
     public class CityContext : DbContext
     {
+        public DbSet<City> Cities { get; set; } //Not nullable!!
+        public DbSet<PointOfInterest> PointsOfInterest { get; set; }
     }
 }
